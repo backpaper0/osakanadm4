30f5ef7aaa3f6dfd952783e81c25bc33f31314bc
Uragami Taichi <backpaper0@gmail.com>
2019-12-25 06:59:28 +0900

    Appをfunctional componentにした

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Photo = (props) => {
+const Photo = props => {
   const [fav, setFav] = useState(props.fav);
   const { image, title } = props;
   const addFav = () => setFav(fav => fav + 1);
@@ -24,37 +24,28 @@ const Photo = (props) => {
   );
 };
 
-class App extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: []
-    };
-  }
-
-  async componentDidMount() {
-    const resp = await fetch("http://localhost:8080/api/photos.json");
-    const json = await resp.json();
-    const photos = json.map(a => ({ ...a, fav: 0 }));
-    this.setState({ photos });
-  }
-
-  render() {
-    const { photos } = this.state;
-    return (
-      <div>
-        <header style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "40px",
-            borderBottom: "1px solid silver", boxSizing: "border-box", display: "flex",
-            alignItems: "center", backgroundColor: "white" }}>
-          <h1 style={{ fontSize: "large", fontStyle: "italic", margin: 0 }}>Osakanagram</h1>
-        </header>
-        <div style={{ width: "500px", margin: "65px auto" }}>
-          {photos.map(a => <Photo {...a}/>)}
-        </div>
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const resp = await fetch("http://localhost:8080/api/photos.json");
+      const json = await resp.json();
+      const photos = json.map(a => ({ ...a, fav: 0 }));
+      setPhotos(photos);
+    })();
+  }, []);
+  return (
+    <div>
+      <header style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "40px",
+          borderBottom: "1px solid silver", boxSizing: "border-box", display: "flex",
+          alignItems: "center", backgroundColor: "white" }}>
+        <h1 style={{ fontSize: "large", fontStyle: "italic", margin: 0 }}>Osakanagram</h1>
+      </header>
+      <div style={{ width: "500px", margin: "65px auto" }}>
+        {photos.map(a => <Photo {...a}/>)}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
